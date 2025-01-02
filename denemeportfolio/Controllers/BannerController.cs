c770a472848b091154dd35bb0b947d5ea76b6e58
narin uluışık <narinuluisik7@gmail.com>
2025-01-02 13:08:32 +0300

    düzenlendi

---
+++ denemeportfolio/Controllers/BannerController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using denemeportfolio.Models;
+
+
+namespace Controllers
+{
+    public class BannerController : Controller
+    {
+        // GET: Testimonial
+        DbPortfolioEntities1 db = new DbPortfolioEntities1();
+        public ActionResult Index()
+        {
+            var values = db.TblBanner.ToList();
+            return View(values);
+
+        }
+        [HttpGet]
+        public ActionResult CreateBanner()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult CreateBanner(TblBanner p)
+        {
+            db.TblBanner.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+        public ActionResult DeleteBanner(int id)
+        {
+            var value = db.TblBanner.Find(id);
+            db.TblBanner.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult UpdateBanner(int id)
+        {
+            var value = db.TblBanner.Find(id);
+
+            return View(value);
+
+        }
+        [HttpPost]
+        public ActionResult UpdateBanner(TblBanner p)
+        {
+            var value = db.TblBanner.Find(p.BannerId);
+            value.Description = p.Description;
+            value.Title = p.Title;
+            value.ImageUrl = p.ImageUrl;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ denemeportfolio/Controllers/HobbyController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using denemeportfolio.Models;
+
+
+namespace Controllers
+{
+    public class HobbyController : Controller
+    {
+        // GET: Testimonial
+        DbPortfolioEntities1 db = new DbPortfolioEntities1();
+        public ActionResult Index()
+        {
+            var values = db.TblHobby.ToList();
+            return View(values);
+
+        }
+        [HttpGet]
+        public ActionResult CreateHobby()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult CreateHobby(TblHobby p)
+        {
+            db.TblHobby.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+        public ActionResult DeleteHobby(int id)
+        {
+            var value = db.TblHobby.Find(id);
+            db.TblHobby.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult UpdateHobby(int id)
+        {
+            var value = db.TblHobby.Find(id);
+
+            return View(value);
+
+        }
+        [HttpPost]
+        public ActionResult UpdateHobby(TblHobby p)
+        {
+            var value = db.TblHobby.Find(p.HobbyId);
+            value.IconUrl = p.IconUrl;
+            value.Title = p.Title;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
